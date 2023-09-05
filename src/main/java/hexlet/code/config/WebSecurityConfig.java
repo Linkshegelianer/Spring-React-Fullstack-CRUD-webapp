4b4299f4ffd7d0f769b5f1f42d6becf35bbb2b00
Lera <mssolipsismus@gmail.com>
2023-09-05 21:24:35 +0300

    Update WebSecurityConfig.java

--- src/main/java/hexlet/code/config/WebSecurityConfig.java
+++ src/main/java/hexlet/code/config/WebSecurityConfig.java
@@ -4,7 +4,6 @@ import hexlet.code.security.JWTAuthenticationFilter;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -22,7 +21,6 @@ import static org.springframework.http.HttpMethod.POST;
 
 @Configuration
 @EnableWebSecurity
-//@EnableGlobalMethodSecurity(prePostEnabled = true)
 @AllArgsConstructor
 public class WebSecurityConfig {
 
